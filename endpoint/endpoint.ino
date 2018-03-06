6a51876c938388c13674cc73b77abfaf86875f7c
SlimTim10 <slimtim10@gmail.com>
2018-03-06 14:46:03 -0500

    Use maybe for error handling

--- endpoint/endpoint.ino
+++ endpoint/endpoint.ino
@@ -1,13 +1,16 @@
 #include <RH_ASK.h>
+#include <maybe.h>
 #include "hal.h"
 
 /* #define DEBUG */
 
 #define die()	while (1)
 
-const char *msg = "win";
+const char *WIN_MESSAGE = "win";
 
-RH_ASK radio(2000, RF_RX, 0);
+static RH_ASK radio(2000, RF_RX, 0);
+static uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+static uint8_t buflen = sizeof(buf);
 
 void setup(void) {
 #	ifdef DEBUG
@@ -28,21 +31,35 @@ void setup(void) {
 }
 
 void loop(void) {
-	uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-	uint8_t buflen = sizeof(buf);
+	maybe_do
+		(mstart,
+		 receive_msg,
+		 handle_win);
+}
 
+/* Receives a radio message */
+/* void -> maybe uint8_t[] */
+static maybe receive_msg(void *empty_) {
 	if (radio.recv(buf, &buflen)) {
 #		ifdef DEBUG
 		radio.printBuffer("Received:", buf, buflen);
 #		endif
-		if (strncmp((const char *) buf, msg, strlen(msg)) == 0) {
-			led_on();
-			delay(1000);
-			led_off();
-		}
+		return mreturn(buf);
+	} else {
+		return nothing();
 	}
+}
 
-	
+/* Handles the winning condition if it is met */
+/* uint8_t[] -> maybe void */
+static maybe handle_win(void *buf_) {
+	uint8_t *msg = (uint8_t *) buf_;
+	if (strncmp((const char *) msg, WIN_MESSAGE, strlen(WIN_MESSAGE)) == 0) {
+		led_on();
+		delay(1000);
+		led_off();
+	}
+	return nothing();
 }
 
 static void led_off(void) {
