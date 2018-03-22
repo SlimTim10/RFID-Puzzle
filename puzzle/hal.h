a21cbea16829f234d7988c857bccdfdf36476015
SlimTim10 <slimtim10@gmail.com>
2018-03-21 22:31:00 -0400

    Two NFC boards working

--- puzzle/hal.h
+++ puzzle/hal.h
@@ -3,7 +3,9 @@
 
 enum pins {
 	RF_TX = 2,
-	NFC_CS = 10,
+	NFC_CS_0 = 9,
+	NFC_CS_1 = 10,
+	NFC_CS_2 = 11,
 };
 
 #endif
--- puzzle/puzzle.ino
+++ puzzle/puzzle.ino
@@ -8,22 +8,38 @@
 
 #define die()	while (1)
 
-enum constants {
-	WIN_TAG_DATA = 0x01,
-};
-
 struct uid {
 	uint8_t val[7];
 	uint8_t length;
 };
 
-const char *WIN_MESSAGE = "win";
+enum constants {
+	NUM_NFC = 2,
+	NFC_INIT_ATTEMPTS = 3,
+};
+
+static const char *WIN_MESSAGE = "win";
+
+static const uint8_t WIN_TAG_DATA[3] = { 0x01, 0x02, 0x03 };
 
 static RH_ASK radio(2000, 0, RF_TX);
-static PN532_SPI pn532spi(SPI, NFC_CS);
-static PN532 nfc(pn532spi);
-struct uid uid = { .val = {0, 0, 0, 0, 0, 0, 0}, .length = 7};
-static uint8_t data[32];
+
+static PN532_SPI pn532spi_0(SPI, NFC_CS_0);
+static PN532 nfc_0(pn532spi_0);
+static PN532_SPI pn532spi_1(SPI, NFC_CS_1);
+static PN532 nfc_1(pn532spi_1);
+static PN532_SPI pn532spi_2(SPI, NFC_CS_2);
+static PN532 nfc_2(pn532spi_2);
+
+static PN532 *nfc[3] = { &nfc_0, &nfc_1, &nfc_2 };
+
+static struct uid uid[3] = {
+	{ .val = {0, 0, 0, 0, 0, 0, 0}, .length = 7 },
+	{ .val = {0, 0, 0, 0, 0, 0, 0}, .length = 7 },
+	{ .val = {0, 0, 0, 0, 0, 0, 0}, .length = 7 },
+};
+static uint8_t buf[32];
+static uint8_t tag_data[3];
 
 void setup(void) {
 #	ifdef DEBUG
@@ -38,14 +54,14 @@ void setup(void) {
 		die();
 	}
 
-	nfc.begin();
-	uint32_t versiondata = nfc.getFirmwareVersion();
-	if (!versiondata) {
-		Serial.println("Didn't find PN53x board");
-		die();
+	uint8_t i;
+	for (i = 0; i < NUM_NFC; i++) {
+		if (!init_nfc(nfc[i])) {
+			Serial.print("Didn't find PN53x board: ");
+			Serial.println(i);
+			die();
+		}
 	}
-
-	nfc.SAMConfig();
 
 #	ifdef DEBUG
 	Serial.println("Ready");
@@ -54,27 +70,33 @@ void setup(void) {
 
 void loop(void) {
 	maybe_do
-		(find_tag,
-		 read_tag,
+		(find_tags,
+		 read_tags,
 		 handle_win);
-	
+
 	delay(500);
 }
 
 /* Finds Mifare Ultralight tag */
-/* void -> maybe uid */
-static maybe find_tag(void *empty_) {
-	uint8_t success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid.val, &uid.length);
+/* void -> maybe uid[] */
+static maybe find_tags(void *empty_) {
+	uint8_t success = true;
+	uint8_t i;
+	for (i = 0; i < NUM_NFC && success; i++) {
+		nfc[i]->getFirmwareVersion(); /* Needed for successful consecutive readings */
+		success = nfc[i]->readPassiveTargetID(PN532_MIFARE_ISO14443A, uid[i].val, &uid[i].length);
+		success &= (uid[i].length == 7);
 
-#	ifdef DEBUG
-	if (success) {
-		Serial.println("UID: ");
-		nfc.PrintHex(uid.val, uid.length);
-		Serial.println("");
+#		ifdef DEBUG
+		if (success) {
+			Serial.println("UID: ");
+			nfc[i]->PrintHex(uid[i].val, uid[i].length);
+			Serial.println("");
+		}
+#		endif
 	}
-#	endif
 
-	if (success && uid.length == 7) {
+	if (success) {
 		return mreturn(&uid);
 	} else {
 		return nothing();
@@ -82,20 +104,24 @@ static maybe find_tag(void *empty_) {
 }
 
 /* Reads Mifare Ultralight tag data to buffer */
-/* void -> maybe uint8_t[] */
-static maybe read_tag(void *empty_) {
-	uint8_t success = nfc.mifareultralight_ReadPage(4, data);
-
-#	ifdef DEBUG
-	if (success) {
-		Serial.println("Reading page 4:");
-		nfc.PrintHexChar(data, 4);
-		Serial.println("");
+/* void -> maybe uint8_t[3] */
+static maybe read_tags(void *empty_) {
+	uint8_t success = true;
+	uint8_t i;
+	for (i = 0; i < NUM_NFC && success; i++) {
+		success = nfc[i]->mifareultralight_ReadPage(4, buf);
+		if (success) {
+			tag_data[i] = buf[0];
+#			ifdef DEBUG
+			Serial.println("Reading page 4:");
+			nfc[i]->PrintHexChar(buf, 4);
+			Serial.println("");
+#			endif
+		}
 	}
-#	endif
 
 	if (success) {
-		return mreturn(&data);
+		return mreturn(&tag_data);
 	} else {
 		return nothing();
 	}
@@ -105,7 +131,7 @@ static maybe read_tag(void *empty_) {
 /* uint8_t[] -> maybe void */
 static maybe handle_win(void *data_) {
 	uint8_t *data = (uint8_t *) data_;
-	if (data[0] == WIN_TAG_DATA) {
+	if (data[0] == WIN_TAG_DATA[0]) {
 		radio.send((uint8_t *) WIN_MESSAGE, strlen(WIN_MESSAGE));
 		radio.waitPacketSent();
 #	   	ifdef DEBUG
@@ -114,4 +140,22 @@ static maybe handle_win(void *data_) {
 #		endif
 	}
 	return nothing();
+}
+
+/* Initializes NFC board */
+static bool init_nfc(PN532 *x) {
+	uint32_t versiondata;
+	versiondata = false;
+	uint8_t i;
+	for (i = 0; i < NFC_INIT_ATTEMPTS && !versiondata; i++) {
+		x->begin();
+		versiondata = x->getFirmwareVersion();
+	}
+	if (!versiondata) {
+		return false;
+	}
+
+	x->SAMConfig();
+	
+	return true;
 }
